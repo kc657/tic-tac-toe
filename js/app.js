83266516cec47412ab7ebad60a6b281fa5cdb63b
kc657 <kvn.0218@gmail.com>
2017-07-03 14:52:06 -0700

    Game logic added in JS

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 // wait for the DOM to finish loading
 $(document).ready(function () {
-  // all code to manipulate the DOM
-  // goes inside this function
+  var playerTurn = ['userInput_click_p1', 'userInput_click_p2', 'userInput_click_p1', 'userInput_click_p2', 'userInput_click_p1', 'userInput_click_p2', 'userInput_click_p1', 'userInput_click_p2', 'userInput_click_p1']
 
+  $('.userInput').click(function () {
+    if (playerTurn.length !== 0) {
+      if (playerTurn.length % 2 !== 0) {
+        $(this).toggleClass(playerTurn[0])
+        console.log(playerTurn[0])
+        playerTurn.splice(0, 1)
+      } else {
+        $(this).toggleClass(playerTurn[0])
+        console.log(playerTurn[0])
+        playerTurn.splice(0, 1)
+      }
+    } else {
+      console.log('Game Over')
+    }
+  })
 })
+
+document.getElementById('myForm').reset()
