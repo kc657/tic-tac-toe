dfaf4dc8d5d05b5d9a569e0986394ac8902b9472
kc657 <kvn.0218@gmail.com>
2017-07-05 03:29:37 -0700

    Last possible move prompts popup for draw

--- js/app.js
+++ js/app.js
@@ -11,7 +11,13 @@ $(document).ready(function() {
               window.alert('Already taken, please select a empty square!')
             } else if ($(this).hasClass('userInput')) {
               if (playerTurn.length !== 0) {
-                if (playerTurn.length % 2 !== 0) {
+                if (playerTurn.length === 1) {
+                  $(this).addClass(playerTurn[0])
+                  $(this).removeClass('userInput')
+                  console.log(playerTurn[0])
+                  playerTurn.splice(0, 1)
+                  window.alert('Draw..or someone won and both of you are blind. Please click the "Reset Game" button to play again.')
+                } else if (playerTurn.length % 2 !== 0) {
                   $(this).addClass(playerTurn[0])
                   $(this).removeClass('userInput')
                   console.log(playerTurn[0])
